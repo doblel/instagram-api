402e9c8f89dbebbffe5325f7a8095809ad6866c2
doblel <lucattilisandro@gmail.com>
2017-02-07 14:07:30 -0300

    [ADD] relationship endpoints

--- instagram/instagram.py
+++ instagram/instagram.py
@@ -10,7 +10,13 @@ class Instagram(object):
     auth_path = '/oauth'
     api_path = host + base_path
 
-    def __init__(self, client_id=None, client_secret=None, redirect_uri=None, access_token=None):
+    def __init__(
+        self,
+        client_id=None,
+        client_secret=None,
+        redirect_uri=None,
+        access_token=None
+    ):
         """Initial args."""
         self.client_id = client_id
         self.client_secret = client_secret
@@ -18,12 +24,13 @@ class Instagram(object):
         self.access_token = access_token
 
     # https://www.instagram.com/developer/authentication/
-    def auth_url(self, scope=None):
+    def auth_url(self, scope='basic+public_content+follower_list'):
         """Return instagram authorization url."""
-        url = self.host + self.auth_path
-        url += '/authorize/?client_id={}&redirect_uri={}&&response_type=code'.format(
+        url = self.host + self.auth_path + '/authorize/?'
+        url += 'client_id={}&redirect_uri={}&response_type=code&scope={}'.format(
             self.client_id,
-            self.redirect_uri
+            self.redirect_uri,
+            scope
         )
         return url
 
@@ -39,7 +46,7 @@ class Instagram(object):
             'code': code
         }
 
-        return self._make_request('POST', url=url, data=payload)
+        return self._make_request(method='POST', url=url, data=payload)
 
     # https://www.instagram.com/developer/endpoints/users/#get_users_self
     def self(self, access_token=None):
@@ -49,7 +56,7 @@ class Instagram(object):
             'access_token': access_token
         }
 
-        return self._make_request('GET', url=url, params=payload)
+        return self._make_request(url=url, params=payload)
 
     # https://www.instagram.com/developer/endpoints/users/#get_users_media_recent_self
     def self_media(self, access_token=None):
@@ -59,7 +66,7 @@ class Instagram(object):
             'access_token': access_token
         }
 
-        return self._make_request('GET', url=url, params=payload)
+        return self._make_request(url=url, params=payload)
 
     # https://www.instagram.com/developer/endpoints/users/#get_users_feed_liked
     def self_media_liked(self, access_token=None):
@@ -69,7 +76,7 @@ class Instagram(object):
             'access_token': access_token
         }
 
-        return self._make_request('GET', url=url, params=payload)
+        return self._make_request(url=url, params=payload)
 
     # https://www.instagram.com/developer/endpoints/users/#get_users
     def user_id(self, user_id=None, access_token=None):
@@ -79,7 +86,7 @@ class Instagram(object):
             'access_token': access_token
         }
 
-        return self._make_request('GET', url=url, params=payload)
+        return self._make_request(url=url, params=payload)
 
     # https://www.instagram.com/developer/endpoints/users/#get_users_media_recent
     def user_media(self, user_id=None, access_token=None):
@@ -101,7 +108,63 @@ class Instagram(object):
             'access_token': access_token
         }
 
-        return self._make_request('GET', url, params=payload)
+        return self._make_request(url=url, params=payload)
+
+    # https://www.instagram.com/developer/endpoints/relationships/#get_relationship
+    def get_user_relationship(self, user_id=None, access_token=None):
+        """Get information about a relationship to another user."""
+        url = self.api_path + '/users/{id}/relationship?'.format(id=user_id)
+        payload = {
+            'access_token': access_token
+        }
+
+        return self._make_request(url=url, params=payload)
+
+    # https://www.instagram.com/developer/endpoints/relationships/#post_relationship
+    def update_relationship(self, user_id=None, action=None, access_token=None):
+        """
+        Modify the relationship between the current user and the target user.
+        You need to include an action parameter to specify the relationship
+        action you want to perform.
+        Valid actions are: 'follow', 'unfollow' 'approve' or 'ignore'.
+        """
+        url = self.api_path + '/users/{id}/relationship?'.format(id=user_id)
+        payload = {
+            'access_token': access_token,
+            'action': action
+        }
+
+        return self._make_request(method='POST', url=url, params=payload)
+
+    # https://www.instagram.com/developer/endpoints/relationships/#get_users_follows
+    def self_follows(self, access_token=None):
+        """Get the list of users this user follows."""
+        url = self.api_path + '/users/self/follows?'
+        payload = {
+            'access_token': access_token
+        }
+
+        return self._make_request(url=url, params=payload)
+
+    # https://www.instagram.com/developer/endpoints/relationships/#get_users_followed_by
+    def self_followers(self, access_token=None):
+        """Get the list of users this user is followed by."""
+        url = self.api_path + '/users/self/followed-by?'
+        payload = {
+            'access_token': access_token
+        }
+
+        return self._make_request(url=url, params=payload)
+
+    # https://www.instagram.com/developer/endpoints/relationships/#get_incoming_requests
+    def self_pending(self, access_token=None):
+        """List the users who have requested this user's permission to follow."""
+        url = self.api_path + '/users/self/requested-by?'
+        payload = {
+            'access_token': access_token
+        }
+
+        return self._make_request(url=url, params=payload)
 
     # internal method
     def _make_request(self, method='GET', url=None, params=None, data=None):
